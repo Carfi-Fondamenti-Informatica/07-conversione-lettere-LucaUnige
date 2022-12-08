39e05ad7410124ea09e49cd034a9848e08d3f58d
Luca Sangiovanni <118013855+LucaUnige@users.noreply.github.com>
2022-12-08 22:41:56 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,22 @@
 #include <iostream>
+using namespace std;
+#include "lib.h"
+int main() {
+    char a;
+    bool t;
+cin>>a;
 
-int main(){
-  return 0;
+t= conversione(a);
+if(t== true){
+    cout<<a<<endl;
+}
+else{
+    cout<<"errore"<<endl;
+}
+
+
+
+
+
+    return 0;
 }
